556d7a5152e9e3693b0b458f58c2b8f00f17bd88
unknown <ithub885@gmail.com>
2023-01-08 08:58:41 -0800

    new up

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { DashComponent } from './pages/dash/dash.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SliderComponent } from './shared/slider/slider.component';
 
 
 @NgModule({
   declarations: [
     AdminComponent,
-    DashComponent
+    DashComponent,
+    HeaderComponent,
+    SliderComponent
   ],
   imports: [
     CommonModule,
